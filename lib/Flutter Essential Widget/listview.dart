241024f9752e097c2a28fd516c1b1795e2ca72fa
Hasib <hasibislam2k18@gmail.com>
2024-05-13 00:40:08 +0600

    add

---
+++ lib/Flutter Essential Widget/listview.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ListViewWidget extends StatelessWidget {
+  const ListViewWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    appBar: AppBar(
+      title: Text('My App'),
+    ),
+    );
+  }
+}
